57ddd85e49dc4f071ad71178fd84ebaf89986340
nico bazzoni <nicobazzoni@gmail.com>
2022-10-17 21:33:19 -0400

    basic layout

---
+++ components/Footer.js
@@ -0,0 +1,18 @@
+import { BsGlobe} from "react-icons/bs";
+
+function Footer() {
+    return <footer>
+        <div className="flex justify-center items-center space-x-4 py-3">
+            <BsGlobe clas sName="h-5" />
+
+            <select name="" id="" className="focus:outline-none">
+                <option value="English">English</option>
+                <option value="Hindi">Hindi</option>
+                <option value="Marathi">Marathi</option>
+            </select>
+        </div>
+
+    </footer>
+}
+
+export default Footer;
--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+
+  images: {
+    domains: ["www.google.co.uk", 'www.unsplash.com']
+  }
 }
 
 module.exports = nextConfig
