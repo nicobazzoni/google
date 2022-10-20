43332421154b3f34b936e588d9e042a6eee6a5af
nico bazzoni <nicobazzoni@gmail.com>
2022-10-19 21:46:33 -0400

    search term injected with useRef

---
+++ pages/api/search.js
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+
+function Search() {
+  return <Head>
+    <title>Search Results</title>
+
+    {/* header */}
+    
+
+
+    {/* results */}
+    <h1>Search</h1>
+  </Head>;
+}
+
+export default Search;
